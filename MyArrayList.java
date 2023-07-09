0c06004cb9e9f3ddadd1cd8851ab04b56062c5cc
LEGION <adietk@bk.ru>
2023-07-09 22:40:41 +0600

    Added the contains(Object o) method to check if an element is in the list.

--- MyArrayList.java
+++ MyArrayList.java
@@ -9,5 +9,12 @@ public class MyArrayList<T>  implements MyList<T> {
         return size;
     }
 
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
