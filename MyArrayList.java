b139bf5c2c57a5eab26bd9a2b585c07e34194a0c
LEGION <adietk@bk.ru>
2023-07-09 22:28:47 +0600

    1.Added interface MyList with a set of methods 2.Added private Object[] elements and private int size fields to store list elements and keep track of its size. 3.The MyArrayList() constructor initializes the elements array with length 10 and sets the initial value of size to 0. 4.Added the size() method, which returns the current size of the list.

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,3 +1,13 @@
-public class MyArrayList {
+public class MyArrayList<T>  implements MyList<T> {
+    private Object[] elements;
+    private int size;
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+    public int size(){
+        return size;
+    }
 
+    }
 }
